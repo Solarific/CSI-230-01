83b2d6d60879af8958d53cf25ace2aed5bfe07f1
root <root@comp.myguest.virtualbox.org>
2023-10-03 21:04:06 -0400

    Apache Log Menu

--- Week5/ApacheLogMenu.bash
+++ Week5/ApacheLogMenu.bash
@@ -7,8 +7,9 @@ input=clientIPs.txt
 
 while file= read -r line
 do
-echo "$line"
-cat /var/log/apache2/access.log | grep $(date +"%d/%b/%Y") | grep "$line" | wc -l  | sed 's/$/ Times found/'
+
+count=$(cat /var/log/apache2/access.log | grep $(date +"%d/%b/%Y") | grep "$line" -c )
+echo "${count}" "$line"
 done < "$input"
 }
 
@@ -18,11 +19,51 @@ input=clientIPs.txt
 while file= read -r line
 do
 
-#x = cat /var/log/apache2/access.log | cut -d ':' -f2
-#y = date +"%d/%b/%Y/%T" | cut -d '/' -f4 | cut -c 1,2
-cat /var/log/apache2/access.log | grep "$line" | egrep $(date +"%d/%b/%Y/%T:0[0-24]") | grep -E '400|403|404' | wc -l 
+count=$(cat /var/log/apache2/access.log | grep "${line}" | grep $(date +"%d/%b/%Y:%H:[0-9][0-9]") | cut -c 50- | grep -c -E  '400|403|404'  )
+
+if [[ "${count}" -ge  3 ]]
+then
+echo -e  "${line}" >> blacklisted.txt
+fi
 
 done < "$input"
+echo "IPS Blacklisted"
+cat blacklisted.txt
+}
+
+histogram ()
+{
+
+count=$(cat /var/log/apache2/access.log | grep $(date +"%d/%b/%Y") | grep "$line" | grep -c "200" | sed 's/$/ Visits today!/' )
+echo "${count}"
+
+}
+
+block () 
+{
+
+input=blacklisted.txt
+
+while file= read -r line
+do
+
+sudo iptables -A INPUT -s "${line}" -j DROP
+
+done < "$input"
+iptables -L -v
+}
+
+resetblocks () 
+{
+
+input=blacklisted.txt
+
+while file= read -r line
+do
+sudo iptables -D INPUT -s "${line}" -j DROP
+
+done < "$input"
+iptables -L -v
 }
 
 userchoice=0
@@ -33,7 +74,7 @@ do
 echo "[1] Number of Visitors"
 echo "[2] Display of Visitors"
 echo "[3] Show Bad Visits"
-echo "[4] Bl9ock Bad Visits"
+echo "[4] Block Bad Visits"
 echo "[5] Reset Block Rules"
 echo "[6] Show Visit Histogram"
 echo "[7] Quit"
@@ -53,6 +94,24 @@ read userchoice
 	if [[ "$userchoice" -eq 3 ]]
         then
                 bad_Clients
+        fi
+        if [[ "$userchoice" -eq 4 ]]
+        then
+                block
+        fi
+
+if [[ "$userchoice" -eq 5 ]]
+        then
+               resetblocks
+        fi
+
+ if [[ "$userchoice" -eq 6 ]]
+        then
+                histogram
+        fi
+ if [[ "$userchoice" -lt 1 || "$userchoice" -gt 7 ]]
+        then
+                echo "invalid choice!, try again!"
         fi
 
 done
