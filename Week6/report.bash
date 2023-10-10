7a7bd1dd5ac8208885ae3fac1d81d70205c6ada4
root <root@comp.myguest.virtualbox.org>
2023-10-10 02:45:00 -0400

    make website

--- Week6/report.bash
+++ Week6/report.bash
@@ -12,34 +12,31 @@ echo "
     <body>
 
 <table border=\"1\"> 
-    <tr>
-        <th>IP</th>
-        <th>Date/Time</th>
-        <th>Page Request</th>
-        <th>UserAgent</th>
-    </tr>
-" >> logs.html
+   <tr>
+   <th> IP </th>
+   <th> Date/Time </th>
+   <th> Page Request </th>
+   <th> UserAgent </th>
+   </tr>" >> logs.html
 
-while File= read -r line
+while file= read -r line
 do
     ip=$(echo "$line" | cut -d ' ' -f 1)
     date=$(echo "$line" |  cut -d '[' -f2 | cut -d '-' -f1)
     pagereq=$(echo "$line" | cut -d '"' -f2 | cut -d ' ' -f 2)
     useragent=$(echo "$line" | cut -d ' ' -f 12)
 
-    echo "
-     <tr>
+echo "<tr>
       <td>"$ip"</td>
       <td>"$date"</td>
       <td>"$pagereq"</td>
       <td>"$useragent"</td>
-     </tr>
-    " >> logs.html
+      </tr> " >> logs.html
 done < mergedlogs.txt
 
 echo "
-        </table>
-    </body>
+</table>
+</body>
 </html>" >> logs.html
 
 cp logs.html /var/www/html/logs.html
