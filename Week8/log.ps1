197d462734c0157ecb3487307cafd026542a9cb4
Solarific <roflwotnoob@live.com>
2023-10-18 13:36:45 -0400

    grab logw

---
+++ Week8/log.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\Users\roflw\Downloads
+clear 
+Get-EventLog -list
+
+$readlog = Read-host -Prompt "Please select a log to review"
+
+Get-Event -LogName $readlog -Newest 3 | export -csv -NoTypeInformation -Path "C:\Users\roflw\Downloads\rep.csv"
